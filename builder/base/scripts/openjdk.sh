f1e9fb3c6542bbfd436765e6836ef00c66e432f4
Klaus Hartl <kh@waymate.de>
2014-04-23 15:49:55 +0200

    Updating Gradle to the latest available version.

--- builder/base/scripts/openjdk.sh
+++ builder/base/scripts/openjdk.sh
@@ -10,12 +10,12 @@ sudo apt-get -y install ant ant-contrib ivy maven
 # install utility that allows us to switch JDK versions
 sudo apt-get -y install default-jdk
 
-# Download gradle 1.3
-wget http://services.gradle.org/distributions/gradle-1.9-bin.zip --quiet
-unzip -q gradle-1.9-bin.zip 
-rm gradle-1.9-bin.zip 
-sudo mv gradle-1.9 /usr/local
-sudo chown -R ubuntu:ubuntu /usr/local/gradle-1.9
-chmod +x /usr/local/gradle-1.9/bin/gradle
+# Download gradle 1.11
+wget http://services.gradle.org/distributions/gradle-1.11-bin.zip --quiet
+unzip -q gradle-1.11-bin.zip
+rm gradle-1.11-bin.zip
+sudo mv gradle-1.11 /usr/local
+sudo chown -R ubuntu:ubuntu /usr/local/gradle-1.11
+chmod +x /usr/local/gradle-1.11/bin/gradle
 
 popd
